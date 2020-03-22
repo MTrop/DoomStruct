2c3c78a10869ba26912ad7ccf65cac53df48037d
mtrop <mtropiano@gmail.com>
2020-03-21 23:41:12 -0400

    Updated copyright.

--- src/main/java/net/mtrop/doom/map/data/flags/HexenThingFlags.java
+++ src/main/java/net/mtrop/doom/map/data/flags/HexenThingFlags.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * Copyright (c) 2015-2020 Matt Tropiano
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v2.1
- * which accompanies this distribution, and is available at
+ * This program and the accompanying materials are made available under the 
+ * terms of the GNU Lesser Public License v2.1 which accompanies this 
+ * distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
  ******************************************************************************/
 package net.mtrop.doom.map.data.flags;
